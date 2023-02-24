04cbd18cfe5e6342e366542d15a75cbf4a3822e5
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-02-24 22:26:30 +0800

    实验性更新：fastcmd使用正则表达式进行检查用户输入是否合法

--- server/src/commands/服务器/fastcmd.js
+++ server/src/commands/服务器/fastcmd.js
@@ -51,12 +51,48 @@ export function fastcmd(core, server, socket, payload) {
       if (temp.length === 0){
         continue
       }
+      if (typeof command.info.fastcmd[i].check === 'object' && command.info.fastcmd[i].check.constructor === RegExp){    //检查是否符合规则，模式：正则表达式
+        if (!check(command.info.fastcmd[i].check,temp.join(' '))){
+          server.reply({
+            cmd:'warn',
+            text:`抱歉，参数 ${command.info.fastcmd[i].name} 不符合规则，请重试。`
+          },socket)
+          return false
+        }
+      }
+      if (typeof command.info.fastcmd[i].check === 'function'){    //检查是否符合规则，模式：自定义函数
+        if (!command.info.fastcmd[i].check(temp.join(' '))){
+          server.reply({
+            cmd:'warn',
+            text:`抱歉，参数 ${command.info.fastcmd[i].name} 不符合规则，请重试。`
+          },socket)
+          return false
+        }
+      }
       fakePayload[command.info.fastcmd[i].name] = temp.join(' ')    //给你吧
       break    //没用了，结束循环吧
     }
     var temp = cmdlist.slice(cut,cut+command.info.fastcmd[i].len)
     if (temp.length === 0){
       continue
+    }
+    if (typeof command.info.fastcmd[i].check === 'object' && command.info.fastcmd[i].check.constructor === RegExp){    //检查是否符合规则，模式：正则表达式
+      if (!check(command.info.fastcmd[i].check,temp.join(' '))){
+        server.reply({
+          cmd:'warn',
+          text:`抱歉，参数 ${command.info.fastcmd[i].name} 不符合规则，请重试。`
+        },socket)
+        return false
+      }
+    }
+    if (typeof command.info.fastcmd[i].check === 'function'){    //检查是否符合规则，模式：自定义函数
+      if (!command.info.fastcmd[i].check(temp.join(' '))){
+        server.reply({
+          cmd:'warn',
+          text:`抱歉，参数 ${command.info.fastcmd[i].name} 不符合规则，请重试。`
+        },socket)
+        return false
+      }
     }
     fakePayload[command.info.fastcmd[i].name] = temp.join(' ')    //截取数组，然后join一下
     cut += command.info.fastcmd[i].len
@@ -65,9 +101,13 @@ export function fastcmd(core, server, socket, payload) {
   if (typeof fakePayload.nick === 'string'){
     fakePayload.nick = fakePayload.nick.replace(/@/g,'')    //删掉昵称里面的 “@”
   }
-  
+
   server.handleData(socket,JSON.stringify(fakePayload))
   return false
+}
+
+export function check(re,text){
+  return re.test(text)
 }
 
 
--- server/src/commands/管理员/speak.js
+++ server/src/commands/管理员/speak.js
@@ -69,7 +69,8 @@ export const info = {
   fastcmd:[
     {
       name:'hash',
-      len:1
+      len:1,
+      check: /^[a-zA-Z0-9/\+]{15}$/
     }
   ]
 };
