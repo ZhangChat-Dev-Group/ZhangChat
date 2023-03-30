8aa365aaecc1880b2e77b88c76880dd6d1e13572
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-03-30 21:33:24 +0800

    合并lockroom和unlockroom

--- server/src/commands/管理员/lockedroomlist.js
+++ server/src/commands/管理员/lockedroomlist.js
@@ -1,7 +1,7 @@
 import * as UAC from '../utility/UAC/_info';
 export async function init(core){
-  if (core.lockedroom === undefined){
-    core.lockedroom = []
+  if (core.lockedrooms === undefined){
+    core.lockedrooms = []
   }
 }
 // module main
@@ -14,9 +14,9 @@ export async function run(core, server, socket, data) {
     },socket)
     return server.police.frisk(socket.address, 10);
   }
-  var replytext= '锁定的房间（不建议长期锁定房间）：\n'
-  for (var i=0; i<core.lockedroom.length; i++) {
-    replytext+=core.lockedroom[i]+'\n'
+  var replytext= '锁定的频道（不建议长期锁定频道）：\n'
+  for (var i=0; i<core.lockedrooms.length; i++) {
+    replytext+=core.lockedrooms[i]+'\n'
   }
   server.reply({
     cmd:"info",
@@ -26,7 +26,7 @@ export async function run(core, server, socket, data) {
 // module meta
 export const info = {
   name: 'lockedroomlist',
-  description: '查看所有被锁定的房间',
+  description: '查看所有被锁定的频道',
   usage: `
     API: { cmd: 'lockedroomlist' }
     文本：以聊天形式发送 /lockedroomlist`,
