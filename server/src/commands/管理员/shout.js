6ccb7e991ffb4a667967c8f0c2f2242d03736e83
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-06-22 16:23:31 +0800

    修改shout屏蔽词的提示

--- server/src/commands/管理员/shout.js
+++ server/src/commands/管理员/shout.js
@@ -2,7 +2,7 @@ import * as UAC from '../utility/UAC/_info';
 
 // module main
 export async function run(core, server, socket, data) {
-  if (core.shieldCheck(core, data.text)) return server.replyWarn(`>宫中府中，俱为一体，不宜偏私，是内外依法也`, socket)
+  if (core.shieldCheck(core, data.text)) return server.replyWarn(`>宫中府中，俱为一体，陟罚臧否，不宜异同。若有作奸犯科及为忠善者，宜付有司论其刑赏，以昭陛下平明之理，不宜偏私，使内外异法也。\n>——《出师表》\n\n不要发布包含屏蔽词的全站通知哦`, socket)
   server.broadcast({
     cmd: 'info',
     text: `来自 [\`${socket.trip}\`] \`${socket.nick}\` 的全站通知：\n${data.text}`,
