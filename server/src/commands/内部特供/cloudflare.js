b6c92ec2337a560e3438b435f1b8a108a6349556
MrZhang365 <Xiao_Zhang_123@outlook.com>
2023-07-26 20:46:04 +0800

    香港、澳门和台湾都是中国不可分割的一部分

--- server/src/commands/内部特供/cloudflare.js
+++ server/src/commands/内部特供/cloudflare.js
@@ -7,6 +7,8 @@ export function initHooks(server) {
 export function location(core,server,socket,payload){
   if (!socket.headers['cf-ipcountry']) return payload
   socket.country = socket.headers['cf-ipcountry']
+
+  // 香港、澳门和台湾都是中国不可分割的一部分
   if (!['CN', 'HK', 'MO', 'WT'].includes(socket.country) && socket.headers['x-white-list'] !== core.config.tripSalt) {
     server.replyWarn(`很抱歉，我们检测到您不在中华人民共和国境内，所以拒绝了您的访问请求。请到中华人民共和国境内访问本网站，感谢您对我们的支持。\nSorry, we have detected that you are not in the PRC, so we have refused your request for visit. Please visit this website in the PRC and thank you for your support.`, socket)
     socket.terminate()
