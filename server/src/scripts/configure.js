e01ae90e43f36e3bda28ee27adceecb7782008d1
MrZhang365 <MrZhang365@outlook.com>
2026-05-15 23:26:26 +0800

    为配置向导添加docker环境监测以配合容器化运行

--- server/src/scripts/configure.js
+++ server/src/scripts/configure.js
@@ -10,6 +10,12 @@ import { join } from 'path';
 import ConfigManager from '../serverLib/ConfigManager';
 import SetupWizard from './configLib/SetupWizard';
 
+if (!!process.env.ZHC_IN_DOCKER && !process.env.ZHC_CONFIG_FORCE) {
+  console.info('This script is running in a docker contain, please mount data volumn instead of executing me.')
+  console.info('If you want to use this script anyway, please set `ZHC_CONFIG_FORCE` to any value.')
+  process.exit(0)
+}
+
 // import and initialize configManager & dependencies
 const serverConfig = new ConfigManager(join(__dirname, '../..'));
 const setup = new SetupWizard(serverConfig);
